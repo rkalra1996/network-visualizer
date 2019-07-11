2985ae32aa2f216fcc8fdea9f0a5173016228e70
neha0305verma <neha0305verma@gmail.com>
2019-07-11 17:56:49 +0530

    sidebar, header, footer component added

--- client/src/app/modules/dashboard/dashboard.module.ts
+++ client/src/app/modules/dashboard/dashboard.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { HeaderComponent } from '../shell/component/header/header.component';
 
 @NgModule({
   declarations: [],
---
+++ client/src/app/modules/shell/shell.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './component/footer/footer.component';
+import { HeaderComponent } from './component/header/header.component';
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
+})
+export class ShellModule { }
