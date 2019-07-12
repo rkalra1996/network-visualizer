f4b04d4f7a8649497355d1dd70016cad326bd3f5
sumitnautiyal-sunbird <Rishabh.Kalra@niit.com>
2019-07-12 12:23:52 +0530

    new feature : Added API for getting the total Nodes and Relationships from the graph

--- routes/graphRoutes.js
+++ routes/graphRoutes.js
@@ -7,12 +7,30 @@ const neo4j = require('./../neo4jDriverUtility');
 router.get('/initialdata', (req, res) => {
     console.log('initial data');
     // get initial data from the data base
-    neo4j.getData().then(response => {
+    neo4j.getData()
+    .then(response => {
         res.send(response);
-    }).catch(err => {
+    })
+    .catch(err => {
         console.log('err occured while sending back ', err);
         res.sendStatus(500);
     });
+});
+
+router.get('/graph/metadata', (req,res) => {
+    console.log('get metadata');
+    neo4j.getMetaData()
+    .then(response => {
+        res.send(response);
+    })
+    .catch(err => {
+        console.log('err occured while sending back meta data', err);
+        res.sendStatus(500);
+    });
+    // get the meta data for the selected graph
+    // Meta data will give the following :
+    // 1. Total Organisations and there names
+    // 2. Total Relationships and there names
 });
 
 module.exports = router;
