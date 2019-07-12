9ad4b76aef1b73cca3b05cbeb756a34da1a906a3
neha0305verma <neha0305verma@gmail.com>
2019-07-12 10:20:10 +0530

    ui init header footer sidebar

--- client/src/app/modules/shared/component/sidebar/sidebar.component.ts
+++ client/src/app/modules/shared/component/sidebar/sidebar.component.ts
@@ -1,16 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import {SuiModule} from 'ng2-semantic-ui';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { SuiMultiSelect } from 'ng2-semantic-ui/dist';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.sass']
+  styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
+  options=[];
+  selected=[];
+  filteredOptions={};
+  @ViewChild('org')
+  org: SuiMultiSelect<any, any>;
   constructor() { }
-
+  
   ngOnInit() {
+    this.filteredOptions=["hello","hi","morning","james","jhon","tom"];
+    this.selected=[];
+    this.options=[];
   }
-
+   
 }
