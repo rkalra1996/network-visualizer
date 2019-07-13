cb8f34756f8209505640a74af1010aea47afb04c
sumitnautiyal-sunbird <Rishabh.Kalra@niit.com>
2019-07-13 09:24:20 +0530

    initial layout of dashboard

---
+++ client/src/app/modules/dashboard/components/graph-visualizer/graph-visualizer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-graph-visualizer',
+  templateUrl: './graph-visualizer.component.html',
+  styleUrls: ['./graph-visualizer.component.scss']
+})
+export class GraphVisualizerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- client/src/app/modules/dashboard/dashboard.module.ts
+++ client/src/app/modules/dashboard/dashboard.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
-import { HeaderComponent } from '../shell/component/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { GraphVisualizerComponent } from './components/graph-visualizer/graph-visualizer.component';
+import { DashboardSidebarComponent } from './components/dashboard-sidebar/dashboard-sidebar.component';
+import { DashboardHeaderComponent } from './components/dashboard-header/dashboard-header.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [MainComponent, GraphVisualizerComponent, DashboardSidebarComponent, DashboardHeaderComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule
