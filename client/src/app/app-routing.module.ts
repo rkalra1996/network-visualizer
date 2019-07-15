3b4d9da14d4c5f9f34894fe3022b379571fd62d9
sumit <sumit.nautiyal@gmail.com>
2019-07-15 13:00:31 +0530

    route change to root

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: 'dashboard',
-    loadChildren: './modules/dashboard/dashboard.module#DashboardModule'
+    path: "",
+    loadChildren: "./modules/dashboard/dashboard.module#DashboardModule"
   }
 ];
 
@@ -12,4 +12,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
