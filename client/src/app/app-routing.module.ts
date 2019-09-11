df4972f2854cc5e4d297f8916400e9acf7d01db9
Rishabh Kalra <Rishabh.Kalra@in.niit.com>
2019-09-11 11:27:22 +0530

    code improvement : removed path for dashboard v1

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,12 +3,8 @@ import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: "v2",
-    loadChildren: "./modules/dashboard/dashboard.module#DashboardModule"
-  },
-  {
-    path: "",
-    loadChildren: "./modules/dashboard-v2/dashboard-v2.module#DashboardModule"
+    path: '',
+    loadChildren: './modules/dashboard-v2/dashboard-v2.module#DashboardModule'
   }
 ];
 
